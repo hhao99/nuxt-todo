2c5f8b2258d56d1659edf9ad47bd53f9aabc1504
hhao99 <hhao99@gmail.con>
2025-01-23 13:51:09 -0600

    tailwindv4 integrated

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -5,13 +5,14 @@ const  links = [
 ]
 </script>
 <template>
-    <div class="flex bg-gray-200 dark:bg-gray-800 dark:text-gray-100 p-4">
-        <div class="flex-1">
-            <h1 class="text-2xl">My App</h1>
-        </div>
-        <div class="flex-1 text-right space-x-4">
-            
-            <NuxtLink v-for="link in links" :key="link.path" :to="link.path" class="hover:underline">{{ link.name }}</NuxtLink> 
+    <div class="container min-w-full min-h-10 bg-gray-100/50 dark:bg-gray-800 dark:text-aliceblue">
+        <div class="flex items-center justify-between p-4">
+            <div class="flex-1">
+                <h1 class="text-2xl">My App</h1>
+            </div>
+            <div class="flex-1 text-right space-x-4">
+                <NuxtLink class="hover:underline" v-for="link in links" :key="link.path" :to="link.path" >{{ link.name }}</NuxtLink>
+            </div>  
         </div>
     </div>
   </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -7,6 +8,9 @@ export default defineNuxtConfig({
   },
   modules: [],
   vite: {
-    plugins: [],
-  }
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['~/assets/css/main.css'],
 })
